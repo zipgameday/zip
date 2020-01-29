cbea218c4a196d72a2b7d02ebef5ea9921fdbfac
Dillon Thompson <dj.thompson715@gmail.com>
2020-01-29 12:46:52 -0600

    removed extra .packages file, added loading spinner for location

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("api key here")
+    GMSServices.provideAPIKey("AIzaSyC-qi8dEKCFP1q3FKu9Faxkabd-lj8ysJw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
