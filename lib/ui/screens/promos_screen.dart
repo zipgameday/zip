5d00ef517a6bb5e97c72d3811d55d1708e167763
David Smith <dns0007@auburn.edu>
2020-02-05 09:51:38 -0600

    Added promo page

---
+++ lib/ui/screens/promos_screen.dart
@@ -0,0 +1,6 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:zip/ui/screens/welcome_screen.dart';
+import 'package:zip/ui/screens/main_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
