27d2f2a98d42783c17a34cfd89e1c8f6ba56b0c9
John Thornton <jthornton1996@gmail.com>
2020-02-20 17:11:27 -0600

    Added cloud function support, applyPromo function for Promos page, and updated Promo page with the function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +8,63 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const applyPromoCode = functions.https.onCall(async (data, context) => {
+  try {
+    const uid = data.uid;
+    const promo_code = data.promo_code;
+    // Get promo's data from firebase
+    var promoRef = await admin
+      .firestore().collection("promos").doc(promo_code).get();
+    if (promoRef.exists) {
+      // Check if reference exists in firebase
+      var promo = promoRef.data(); // Get the Object
+      // Get user's data and update credits with
+      var userRef = await admin.firestore().collection("users").doc(uid).get();
+      var user = userRef.data();
+      console.log(promo.credits);
+      console.log(promo.message);
+      if (promo.expiration.toDate() > new Date()) {
+        // Compare expiration date
+        if (!promo.redeemed_users.includes(uid)) {
+          admin
+            .firestore().collection("users").doc(uid)
+            .update({
+              credits: user.credits + promo.credits
+            });
+          // Update Promo's object with the user's id
+          await admin
+            .firestore().collection("promos").doc(promo_code)
+            .update({
+              redeemed_users: admin.firestore.FieldValue.arrayUnion(uid)
+            });
+          return {
+            result: true,
+            message: promo.message
+          };
+        } else {
+          return {
+            result: false,
+            message: "You have already redeemed this code"
+          };
+        }
+      } else {
+        return {
+          result: false,
+          message: "Promo Code has expired"
+        };
+      }
+    } else {
+      return {
+        result: false,
+        message: "Invalid Promo Code"
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      result: false,
+      message: "Server Error: Please try again later."
+    };
+  }
+});
--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:zip/business/auth.dart';
 import 'package:zip/business/user.dart';
 import 'package:zip/models/user.dart';
